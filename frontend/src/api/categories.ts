31014db284687b3095ee3a8b659a2dad885a3e67
AnCichocka <annacichocka.poczta@gmail.com>
2024-05-15 00:06:20 +0200

    change spelling

--- frontend/src/api/categories.ts
+++ frontend/src/api/categories.ts
@@ -9,7 +9,7 @@ export const getCategory = (id: string): Category | undefined => {
 };
 
 const categories: Category[] = [
-  { id: "1", name: "Labolatorium" },
+  { id: "1", name: "Laboratorium" },
   { id: "2", name: "Kartkówka" },
   { id: "3", name: "Wykład" },
   { id: "4", name: "Event" },
