4642cf835f68ddf7d9af05ece735791696b02724
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-06-02 18:31:18 +0200

    Added permissions for teacher

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 
 dependencyManagement {
 	imports {
-		mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release")
+		mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.7.1")
 	}
 }
 
