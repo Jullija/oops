5235d985535df2e2a1d6037eb7bb340eeb044faf
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-06-07 00:28:30 +0200

    Added hall-of-fame view

--- backend/src/main/resources/db/migration/V6__add_edition_table.sql
+++ backend/src/main/resources/db/migration/V6__add_edition_table.sql
@@ -12,17 +12,17 @@ ALTER TABLE groups
     ADD COLUMN edition_id BIGINT NOT NULL REFERENCES edition(edition_id);
 
 ALTER TABLE levels
-    ADD COLUMN edition_id BIGINT NOT NULL REFERENCES edition(edition_id);
+    ADD COLUMN edition_id BIGINT CREATE TABLE award_edition (
+                               award_id BIGINT NOT NULL REFERENCES award(award_id),
+                               edition_id BIGINT NOT NULL REFERENCES edition(edition_id),
+                               PRIMARY KEY (award_id, edition_id)
+);
+NOT NULL REFERENCES edition(edition_id);
 
 ALTER TABLE subcategories
     ADD COLUMN edition_id BIGINT NOT NULL REFERENCES edition(edition_id);
 
 -- Create many-to-many relationship between award and edition
-CREATE TABLE award_edition (
-                               award_id BIGINT NOT NULL REFERENCES award(award_id),
-                               edition_id BIGINT NOT NULL REFERENCES edition(edition_id),
-                               PRIMARY KEY (award_id, edition_id)
-);
 
 ALTER TABLE groups
     DROP COLUMN group_year;
