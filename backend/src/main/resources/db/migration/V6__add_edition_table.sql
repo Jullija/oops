b745e25f1f9176bb4b4b1c209671dd8cd39c7d61
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-06-08 19:54:13 +0200

    Fixed V6 migration

--- backend/src/main/resources/db/migration/V6__add_edition_table.sql
+++ backend/src/main/resources/db/migration/V6__add_edition_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE edition (
-     edition_id SERIAL PRIMARY KEY,
-     name VARCHAR(256) NOT NULL,
-     edition_year INT NOT NULL,
-     label VARCHAR(256) NOT NULL
+                         edition_id SERIAL PRIMARY KEY,
+                         name VARCHAR(256) NOT NULL,
+                         edition_year INT NOT NULL,
+                         label VARCHAR(256) NOT NULL
 );
 
 ALTER TABLE chests
@@ -12,17 +12,17 @@ ALTER TABLE groups
     ADD COLUMN edition_id BIGINT NOT NULL REFERENCES edition(edition_id);
 
 ALTER TABLE levels
-    ADD COLUMN edition_id BIGINT CREATE TABLE award_edition (
-                               award_id BIGINT NOT NULL REFERENCES award(award_id),
-                               edition_id BIGINT NOT NULL REFERENCES edition(edition_id),
-                               PRIMARY KEY (award_id, edition_id)
-);
-NOT NULL REFERENCES edition(edition_id);
+    ADD COLUMN edition_id BIGINT NOT NULL REFERENCES edition(edition_id);
 
 ALTER TABLE subcategories
     ADD COLUMN edition_id BIGINT NOT NULL REFERENCES edition(edition_id);
 
 -- Create many-to-many relationship between award and edition
+CREATE TABLE award_edition (
+                               award_id BIGINT NOT NULL REFERENCES award(award_id),
+                               edition_id BIGINT NOT NULL REFERENCES edition(edition_id),
+                               PRIMARY KEY (award_id, edition_id)
+);
 
 ALTER TABLE groups
     DROP COLUMN group_year;
