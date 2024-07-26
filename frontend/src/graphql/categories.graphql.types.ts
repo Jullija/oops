628af740cb602afb49cbaad741a71591e7e5f4a7
Anna Cichocka <115690306+AnCichocka@users.noreply.github.com>
2024-07-26 17:53:27 +0200

    [40] Integrate form points with backend data (#32)
    * do not support max points upper bound
    
    * add max points validation
    
    * clean up
    
    * delete mockup functions

---
+++ frontend/src/graphql/categories.graphql.types.ts
@@ -0,0 +1,98 @@
+import * as Types from "../__generated__/schema.graphql.types";
+
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
+const defaultOptions = {} as const;
+export type CategoriesQueryVariables = Types.Exact<{ [key: string]: never }>;
+
+export type CategoriesQuery = {
+  __typename?: "query_root";
+  categories: Array<{
+    __typename?: "Categories";
+    categoryId: string;
+    categoryName: string;
+    subcategories: Array<{
+      __typename?: "Subcategories";
+      subcategoryId: string;
+      subcategoryName: string;
+      maxPoints: number;
+    }>;
+  }>;
+};
+
+export const CategoriesDocument = gql`
+  query categories {
+    categories(orderBy: { categoryName: ASC }) {
+      categoryId
+      categoryName
+      subcategories(orderBy: { subcategoryName: ASC }) {
+        subcategoryId
+        subcategoryName
+        maxPoints
+      }
+    }
+  }
+`;
+
+/**
+ * __useCategoriesQuery__
+ *
+ * To run a query within a React component, call `useCategoriesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useCategoriesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useCategoriesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useCategoriesQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    CategoriesQuery,
+    CategoriesQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<CategoriesQuery, CategoriesQueryVariables>(
+    CategoriesDocument,
+    options,
+  );
+}
+export function useCategoriesLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    CategoriesQuery,
+    CategoriesQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<CategoriesQuery, CategoriesQueryVariables>(
+    CategoriesDocument,
+    options,
+  );
+}
+export function useCategoriesSuspenseQuery(
+  baseOptions?: Apollo.SuspenseQueryHookOptions<
+    CategoriesQuery,
+    CategoriesQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useSuspenseQuery<CategoriesQuery, CategoriesQueryVariables>(
+    CategoriesDocument,
+    options,
+  );
+}
+export type CategoriesQueryHookResult = ReturnType<typeof useCategoriesQuery>;
+export type CategoriesLazyQueryHookResult = ReturnType<
+  typeof useCategoriesLazyQuery
+>;
+export type CategoriesSuspenseQueryHookResult = ReturnType<
+  typeof useCategoriesSuspenseQuery
+>;
+export type CategoriesQueryResult = Apollo.QueryResult<
+  CategoriesQuery,
+  CategoriesQueryVariables
+>;
