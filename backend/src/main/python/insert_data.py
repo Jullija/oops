a665498aa5524e5e0206d5eee4904d50f2f97957
Anna Cichocka <115690306+AnCichocka@users.noreply.github.com>
2024-07-27 14:29:21 +0200

    fix (#33)

--- backend/src/main/python/insert_data.py
+++ backend/src/main/python/insert_data.py
@@ -129,7 +129,7 @@ def insert_data(data_count_multiplier=1):
     for year, count in year_group_counts.items():
         for i in range(1, count + 1):
             group_name = f"gr_{i}"
-            cursor.execute("INSERT INTO groups (group_name, edition_id, label, weekday, start_time, end_time) VALUES (%s, %s, %s, %s, %s) RETURNING groups_id",
+            cursor.execute("INSERT INTO groups (group_name, edition_id, label, weekday, start_time, end_time) VALUES (%s, %s, %s, %s, %s, %s) RETURNING groups_id",
                            (group_name, editions[year], "", random.choice(weekdays), "16:00:00", "17:30:00"))
             groups.append(cursor.fetchone()[0])
 
