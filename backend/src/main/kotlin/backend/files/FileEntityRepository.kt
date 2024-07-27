3f1503f1c116a17d24a70f3e930fbe63a30d0adb
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-07-28 01:38:51 +0200

    Added a mutation assignAvatarToUser

--- backend/src/main/kotlin/backend/files/FileEntityRepository.kt
+++ backend/src/main/kotlin/backend/files/FileEntityRepository.kt
@@ -3,5 +3,5 @@ package backend.files
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FileEntityRepository : JpaRepository<FileEntity, Long>{
-
+    fun findByFileId(fileId: Long): FileEntity?
 }
