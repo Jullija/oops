799b14af2bcb9ebbbaee7dd07efae2a276e73e8d
unknown <szymtos@gmail.com>
2024-08-26 02:18:53 +0200

    Added addAwardToEdition

--- backend/src/main/kotlin/backend/awardEdition/AwardEditionRepository.kt
+++ backend/src/main/kotlin/backend/awardEdition/AwardEditionRepository.kt
@@ -1,6 +1,7 @@
 package backend.awardEdition
 
 import backend.award.Award
+import backend.edition.Edition
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -8,5 +9,6 @@ import java.util.*
 @Repository
 interface AwardEditionRepository : JpaRepository<AwardEdition, Long> {
     fun findByAward(award: Award): List<AwardEdition>
+    fun existsByAward_AwardNameAndEdition(awardName: String, edition: Edition): Boolean
 
 }
