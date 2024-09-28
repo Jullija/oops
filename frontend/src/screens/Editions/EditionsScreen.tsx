6f360f3c7caae4228d7338e222c3a17ca4f5d158
AnCichocka <annacichocka.poczta@gmail.com>
2024-09-28 21:26:54 +0200

    improve styling

--- frontend/src/screens/Editions/EditionsScreen.tsx
+++ frontend/src/screens/Editions/EditionsScreen.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useEditionsQuery } from "../../graphql/editions.graphql.types";
 import { pathsGenerator } from "../../router/paths";
+import { Styles } from "../../utils/Styles";
 
 export const EditionsScreen = () => {
   const navigate = useNavigate();
@@ -10,9 +11,10 @@ export const EditionsScreen = () => {
   if (error) return <div>ERROR: {error?.message}</div>;
 
   return (
-    <div>
+    <div style={styles.container}>
       {data?.edition.map((edition) => (
         <div
+          style={styles.card}
           key={edition.editionId}
           onClick={() =>
             navigate(pathsGenerator.coordinator.Edition(edition.editionId))
@@ -23,4 +25,17 @@ export const EditionsScreen = () => {
       ))}
     </div>
   );
+};
+
+const styles: Styles = {
+  container: {
+    display: "flex",
+    gap: 12,
+    flexDirection: "row",
+    margin: 12,
+  },
+  card: {
+    border: "1px solid black",
+    padding: 12,
+  },
 };
