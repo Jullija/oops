f32518c7409d4a231dcf9b3a605bda27680481b6
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-10-21 23:46:50 +0200

    EXTERMINATE ROLES

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,13 +1,6 @@
 import { User } from "../contexts/userContext";
 import { UsersRolesType } from "../__generated__/schema.graphql.types";
 
-export enum Roles {
-  COORDINATOR = "coordinator",
-  STUDENT = "student",
-  TEACHER = "teacher",
-  UNAUTHENTICATED_USER = "unauthenticated_user",
-}
-
 export const defaultUnauthenticatedUser: User = {
   nick: "Guest",
   role: UsersRolesType.UnauthenticatedUser,
