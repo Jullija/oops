382b0f49d1232fb09f283af26d3c89d25dc94bbe
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-10-21 14:30:05 +0200

    CleanedUp imports

--- backend/src/main/kotlin/backend/graphql/FileDataFetcher.kt
+++ backend/src/main/kotlin/backend/graphql/FileDataFetcher.kt
@@ -1,61 +1,20 @@
 package backend.graphql
 
-import backend.award.AwardType
-import backend.bonuses.BonusesRepository
-import backend.categories.Categories
 import backend.categories.CategoriesRepository
-import backend.edition.EditionRepository
 import backend.files.FileEntity
 import backend.files.FileEntityRepository
-import backend.files.FileRetrievalService
-import backend.files.FileUploadService
-import backend.groups.GroupsRepository
-import backend.levels.Levels
+
 import backend.points.PointsRepository
-import backend.subcategories.SubcategoriesRepository
-import backend.userGroups.UserGroups
-import backend.userGroups.UserGroupsRepository
-import backend.userLevel.UserLevelRepository
-import backend.users.FirebaseUserService
 import backend.users.UsersRepository
-import backend.users.Users
-import backend.users.UsersRoles
-import backend.utils.CsvReader
 import backend.utils.UserMapper
-import com.google.firebase.ErrorCode
-import com.google.firebase.auth.FirebaseAuthException
 import com.netflix.graphql.dgs.DgsComponent
-import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
-import org.apache.catalina.User
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.transaction.annotation.Transactional
-import java.math.BigDecimal
-import java.math.RoundingMode
-import java.time.LocalDateTime
-import kotlin.math.exp
-import kotlin.math.min
 
 @DgsComponent
 class FileDataFetcher {
-
-    @Autowired
-    private lateinit var userLevelRepository: UserLevelRepository
-
-    @Autowired
-    private lateinit var groupsRepository: GroupsRepository
-
-    @Autowired
-    private lateinit var subcategoriesRepository: SubcategoriesRepository
-
-    @Autowired
-    private lateinit var editionRepository: EditionRepository
-
-    @Autowired
-    private lateinit var bonusesRepository: BonusesRepository
-
     @Autowired
     lateinit var usersRepository: UsersRepository
 
@@ -69,22 +28,10 @@ class FileDataFetcher {
     lateinit var photoAssigner: PhotoAssigner
 
     @Autowired
-    lateinit var firebaseUserService: FirebaseUserService
-
-    @Autowired
     lateinit var fileEntityRepository: FileEntityRepository
 
     @Autowired
-    lateinit var userGroupsRepository: UserGroupsRepository
-
-    @Autowired
     lateinit var userMapper: UserMapper
-
-    @Autowired
-    lateinit var csvReader: CsvReader
-
-    @Value("\${constants.emailDomain}")
-    lateinit var emailDomain: String
 
     @DgsQuery
     @Transactional
